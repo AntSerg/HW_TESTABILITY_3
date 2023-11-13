80edd9d76fc5845e1bb1c2d9f086608a1dc38d4c
Anton Sergeev <sergant3082605@mail.com>
2023-11-13 23:43:29 +0400

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    int calculate (double sumOfCredit, double percent, int period) {
-        return (int)((sumOfCredit * (percent / 100) / 12) / (1 - 1 / Math.pow(1 + (percent / 100) / 12, period * 12)));
+    public int calculate(double sumOfCredit, double percent, int period) {
+        return (int) ((sumOfCredit * (percent / 100) / 12) / (1 - 1 / Math.pow(1 + (percent / 100) / 12, period * 12)));
     }
 }
